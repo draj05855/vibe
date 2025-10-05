68015af4c2fce5100bca2eb922200335e5e851d2
Raj kumar <draj05855@gmail.com>
2025-10-06 00:39:25 +0530

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,22 @@
+import {  gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    //Imagine this is a dowload step
-    await step.sleep("wait-a-moment", "30s");
-    //Imagine this is a transcript step
-    await step.sleep("wait-a-moment", "10s");
-    //Imagine this is a  summary
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "codeagent",
+      system:
+        "You are an expert next.js developer. You write reddable, maintainable code. you write simple next.js & React snnipets.",
+      model: gemini({ model: "gemini-2.5-flash" }), // model ka naam bhi Gemini ke hisab se badal jayega
+    });
+
+    const { output } = await codeAgent.run(
+  `Write the following snnipet: ${event.data.value}`,
+);
+console.log(output);
+
+    return { output };
+  }
 );
